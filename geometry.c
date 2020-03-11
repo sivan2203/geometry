52a38d3f4f2a4b183b46689a53869e719a54d22e
Ivan <sivan9458@gmail.com>
2020-03-11 10:37:18 +0700

    Fixed geometry.c

--- geometry.c
+++ geometry.c
@@ -3,83 +3,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//Вычесление сторон треугольника
-float Sidelength(int a, int b, int c, int d)
-{
-    float q = ((c - a) * (c - a) + (d - b) * (d - b));
-    float l = sqrt(q);
 
-    return l;
-}
-//Проверка координат треугольника
-void proverka(int* x1, int* y1, int* x2, int* y2, int* x3, int* y3)
-{
-    while (*x1 == *x2 || *x1 == *x3 || *x2 == *x3) {
-        printf("Введите новый параметр x1, x2, x3\n");
-        scanf("%d %d %d", &*x1, &*x2, &*x3);
-    }
-
-    while (*y1 == *y2 || *y1 == *y3 || *y2 == *y3) {
-        printf("Введите новый параметр y1, y2, y3\n");
-        scanf("%d %d %d", &*y1, &*y2, &*y3);
-    }
+float SideLength(int a, int b, int c, int d) {
+  float l = sqrt((c - a) * (c - a) + (d - b) * (d - b));
+  return l;
 }
 
-int main()
-{
-    int x1, y1, r;
-    printf("     x y r \n");
-    printf("Круг ");
-    scanf("%d %d %d", &x1, &y1, &r);
-    int xt1, xt2, yt1, yt2, xt3, yt3;
-    //Проверка радиуса окружности
-    int a = isalpha(x1);
-    int b = isalpha(y1);
-    int c = isalpha(r);
+void cheking(int* x1, int* y1, int* x2, int* y2, int* x3, int* y3) {
+  while (*x1 == *x2 || *x1 == *x3 || *x2 == *x3) {
+    printf("Введите новые координаты для x1, x2, x3:\n");
+    scanf("%d %d %d", &*x1, &*x2, &*x3);
+  }
 
-    if (a != 0 || b != 0 || c != 0) {
-        exit(1);
-    }
+  while (*y1 == *y2 || *y1 == *y3 || *y2 == *y3) {
+    printf("Введите новые координаты для y1, y2, y3:\n");
+    scanf("%d %d %d", &*y1, &*y2, &*y3);
+  }
+}
 
-    while (r <= 0) {
-        printf("Введите другое число больше 0  ");
-        scanf("%d", &r);
-    }
-    //Вычесление периметра круга
-    float Perk = 2 * M_PI * r;
-    printf("Периметр окружности равен: %f \n", Perk);
-    //Вычесление площади круга
-    float Plosk = M_PI * r * r;
-    printf("Площадь круга равна: %f\n", Plosk);
+int main() {
+  int x1, y1, r;
+  printf("Введите начальную координату круга и его радиус x y r:\n");
+  scanf("%d %d %d", &x1, &y1, &r);
+  int xt1, xt2, yt1, yt2, xt3, yt3;
 
-    printf("            x1 y1 x2 y2 x3 y3 \n");
-    printf("Треугольник ");
-    scanf("%d %d %d %d %d %d", &xt1, &yt1, &xt2, &yt2, &xt3, &yt3);
+  int a = isalpha(x1);
+  int b = isalpha(y1);
+  int c = isalpha(r);
 
-    int q = isalpha(xt1);
-    int w = isalpha(yt1);
-    int e = isalpha(xt2);
-    int s = isalpha(yt2);
-    int t = isalpha(xt3);
-    int y = isalpha(yt3);
+  if (a != 0 || b != 0 || c != 0) {
+    exit(1);
+  }
 
-    if (q != 0 || w != 0 || e != 0 || s != 0 || t != 0 || y != 0) {
-        exit(1);
-    }
+  while (r <= 0) {
+    printf("Введите другое число больше 0  ");
+    scanf("%d", &r);
+  }
 
-    proverka(&xt1, &yt1, &xt2, &yt2, &xt3, &yt3);
-    //вычисление 3х сторон треугольника
-    float ab = Sidelength(xt1, yt1, xt2, yt2);
-    float ac = Sidelength(xt1, yt1, xt3, yt3);
-    float bc = Sidelength(xt2, yt2, xt3, yt3);
-    //Вычисление периметра треугольника
-    float Per_t = ab + ac + bc;
-    printf("Периметр треугольника равен: %f\n", Per_t);
-    //вычесление площали треугольника
-    float polyper = Per_t / 2;
-    float Plos_t = polyper * sqrt(sqrt(polyper - ab)) + (sqrt(polyper - ac))
-            + (sqrt(polyper - bc));
-    printf("Площадь треугольника равна: %f\n", Plos_t);
+  //Вычесление периметра круга
 
-    return 0;
+  float Perk = 2 * M_PI * r;
+  printf("Периметр окружности равен: %f \n", Perk);
+
+  //Нахождение площади круга
+
+  float Plosk = M_PI * r * r;
+  printf("Площадь круга равна: %f\n", Plosk);
+
+  printf("Введите: x1 y1 x2 y2 x3 y3 \n");
+  printf("Треугольник ");
+  scanf("%d %d %d %d %d %d", &xt1, &yt1, &xt2, &yt2, &xt3, &yt3);
+
+  int q = isalpha(xt1);
+  int w = isalpha(yt1);
+  int e = isalpha(xt2);
+  int s = isalpha(yt2);
+  int t = isalpha(xt3);
+  int y = isalpha(yt3);
+
+  if (q != 0 || w != 0 || e != 0 || s != 0 || t != 0 || y != 0) {
+    exit(1);
+  }
+
+  cheking(&xt1, &yt1, &xt2, &yt2, &xt3, &yt3);
+
+  //вычисление 3х сторон треугольника
+
+  float ab = Sidelength(xt1, yt1, xt2, yt2);
+  float ac = Sidelength(xt1, yt1, xt3, yt3);
+  float bc = Sidelength(xt2, yt2, xt3, yt3);
+
+  //Вычисление периметра треугольника
+
+  float Per_t = ab + ac + bc;
+  printf("Периметр треугольника равен: %f\n", Per_t);
+
+  //вычесление площали треугольника
+
+  float polyper = Per_t / 2;
+  float Plos_t = polyper * sqrt(sqrt(polyper - ab)) + (sqrt(polyper - ac)) +
+                 (sqrt(polyper - bc));
+  printf("Площадь треугольника равна: %f\n", Plos_t);
+
+  return 0;
 }
